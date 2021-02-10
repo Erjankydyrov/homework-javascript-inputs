c2e8d81b0fcee4a5ce1b73f2639b6d723fc0207a
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-02-10 13:49:28 +0600

    added example1-keyup

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 // example-1
 let example1 = document.querySelector("#exa1");
 let text1 = document.querySelector("#h1");
-
+example1.addEventListener("keyup", function(event) {
+    
+});
 // example-1
 
 // example-2
